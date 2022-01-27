bbf2b633602b7c32c49f90391be6099c50d41469
GowthamRaj K <43011442+gowthamrajk@users.noreply.github.com>
2022-01-27 13:59:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+// for Drag & Drop Logic
+/* 
+function drop(ev) {
+   ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+   ev.target.appendChild(document.getElementById(data));
+   document.getElementById("heading-content").innerHTML = "Dragged & Dropped Successfully !!!";
+   document.getElementById("heading-content").style.color = "green";
+} 
+*/
+
+// for Drag & Copy Logic
+
+function drop(ev) {
+  ev.preventDefault();
+  var data=ev.dataTransfer.getData("text");
+  var nodeCopy = document.getElementById(data).cloneNode(true);
+  nodeCopy.id = "newId";
+  ev.target.appendChild(nodeCopy);
+  document.getElementById("heading-content").innerHTML = "Dragged & Copied Successfully !!!";
+  document.getElementById("heading-content").style.color = "green";
+}
